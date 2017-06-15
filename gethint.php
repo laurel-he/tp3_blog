854f08089c62e671c0b920d7e3c8653cb6f557f8
Laurel(贺小娇) <Laurel_he@163.com>
2017-06-15 20:27:46 +0800

    响应
    这是一个直接响应

---
+++ gethint.php
@@ -0,0 +1,43 @@
+<?php
+// 用名字来填充数组
+$dsn = "mysql:host=localhost;dbname=tp3";
+$db = new PDO($dsn, 'root', 'root');
+$rs = $db->query("SELECT user_name FROM think_users");
+$result_arr = $rs->fetchAll();
+print_r($result_arr);
+$q=$_GET["q"];
+
+//如果 q 大于 0，则查找数组中的所有提示
+if (strlen($q) > 0)
+  {
+  $hint="";
+  for($i=0; $i<count($result_arr); $i++)
+    {
+    if (strtolower($q)==strtolower($result_arr[$i][user_name]))
+      {
+      if ($hint=="")
+        {
+        $hint=$result_arr[$i];
+        }
+      else
+        {
+        $hint=$hint." , ".$result_arr[$i];
+        }
+      }
+    }
+  }
+
+// 如果未找到提示，则把输出设置为 "no suggestion"
+// 否则设置为正确的值
+if ($hint == "")
+  {
+  $response="no suggestion";
+  }
+else
+  {
+  $response=$hint;
+  }
+
+//输出响应
+echo $response;
+?>
