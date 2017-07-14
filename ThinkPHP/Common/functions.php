3a6b9b64b8af103fe805711311276d79dfba2251
Laurel(贺小娇) <Laurel_he@163.com>
2017-07-14 23:28:29 +0800

    2017_07_14
    今天只完成了中文截取乱码问题的操作，而且还有部分问题，感觉挺失败的，明天继续

--- ThinkPHP/Common/functions.php
+++ ThinkPHP/Common/functions.php
@@ -50,7 +50,33 @@ function C($name=null, $value=null,$default=null) {
     }
     return null; // 避免非法参数
 }
-
+/**
+截取中文字符串函数
+*/
+function msubstr($str, $start=0, $length, $charset="utf-8", $suffix=true)  
+    {  
+  if(function_exists("mb_substr")){  
+              if($suffix)  
+              return mb_substr($str, $start, $length, $charset)."...";  
+              else
+                   return mb_substr($str, $start, $length, $charset);  
+         }  
+         elseif(function_exists('iconv_substr')) {  
+             if($suffix)  
+                  return iconv_substr($str,$start,$length,$charset)."...";  
+             else
+                  return iconv_substr($str,$start,$length,$charset);  
+         }  
+         $re['utf-8']   = "/[x01-x7f]|[xc2-xdf][x80-xbf]|[xe0-xef]
+                  [x80-xbf]{2}|[xf0-xff][x80-xbf]{3}/";  
+         $re['gb2312'] = "/[x01-x7f]|[xb0-xf7][xa0-xfe]/";  
+         $re['gbk']    = "/[x01-x7f]|[x81-xfe][x40-xfe]/";  
+         $re['big5']   = "/[x01-x7f]|[x81-xfe]([x40-x7e]|xa1-xfe])/";  
+         preg_match_all($re[$charset], $str, $match);  
+         $slice = join("",array_slice($match[0], $start, $length));  
+         if($suffix) return $slice."…";  
+         return $slice;
+    }
 /**
  * 加载配置文件 支持格式转换 仅支持一级配置
  * @param string $file 配置文件名
